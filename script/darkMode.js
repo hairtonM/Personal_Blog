f0376681300479f901fca57a3ff246d30187fe33
Hairton <hmblogtech@gmail.com>
2020-09-19 15:05:22 -0500

    Mejorado mis redes sociales en especifico los iconos

--- script/darkMode.js
+++ script/darkMode.js
@@ -3,12 +3,14 @@
 */
 
 
-// constantes 
+// ---------------- constantes ----------------
 
 const btnDarkMode = idQuery('.btn_dark_mode')
 const body = idQuery('body')
 
-// functions 
+
+
+// ---------------- functions ----------------
 
 // usando localStorage para mantener el modo oscuro activo asi el usuario haga un refres en la ventana del navegador 
 
@@ -27,7 +29,7 @@ function store(estado){
     localStorage.setItem('darkmode', estado)
 }
 
-// events 
+// ---------------- events ---------------- 
 
 load()
 
@@ -35,6 +37,6 @@ btnDarkMode.addEventListener('click', () => {
 
     body.classList.toggle('darkmode') // add & remove the class 'darkmode' when click the button "Dark Mode"
     store(body.classList.contains('darkmode')) // contains('name of class') me devuelve true si la clase darkmode esta en body y false si no se encuentra
-
-    // log(body.classList.contains('darkmode'))
 })
+
+
